43030704cca31130c21de66dba793d45ac568865
AlexLis312 <metalstyle545@gmail.com>
2026-03-15 19:11:44 +0200

    push

--- server/routes/heroes.js
+++ server/routes/heroes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getRandomHero } from "../controllers/heroesController.js";
+import { getRandomHero, getHeroes } from "../controllers/heroesController.js";
 
 const router = express.Router();
 
 router.get("/random", getRandomHero);
+router.get("/heroes", getHeroes);
 
 export default router;
