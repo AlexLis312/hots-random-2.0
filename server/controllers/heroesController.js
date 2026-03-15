125f7dfe4347adfc5e523740182e5344748fac3f
AlexLis312 <metalstyle545@gmail.com>
2026-03-15 12:23:14 +0200

    push

--- server/controllers/heroesController.js
+++ server/controllers/heroesController.js
@@ -10,3 +10,20 @@ export const getHeroes = async (req, res) => {
     res.status(500).send("DB error");
   }
 };
+
+// RANDOM
+
+export const getRandomHero = async (req, res) => {
+  try {
+    const hero = await sql`
+      SELECT * FROM heroes
+      ORDER BY RANDOM()
+      LIMIT 1
+    `;
+
+    res.json(hero[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("DB error");
+  }
+};
--- server/routes/heroes.js
+++ server/routes/heroes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getHeroes } from "../controllers/heroesController.js";
+import { getHeroes, getRandomHero } from "../controllers/heroesController.js";
 
 const router = express.Router();
 
 router.get("/", getHeroes);
+router.get("/random", getRandomHero);
 // router.post("/", createUser);
 // router.put("/:id", updateHeroes);
 // router.delete("/:id", deleteUser);
